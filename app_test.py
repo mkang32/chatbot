40a0558f2eabb0820cf7d39c16f0b8c38da56d61
Minkyung Kang <minkyung.kang32@gmail.com>
2021-06-29 19:31:44 -0400

    fix typo

--- app_test.py
+++ app_test.py
@@ -5,6 +5,6 @@ url = 'http://127.0.0.1:5000/api/'
 
 data = 'How can you help?'
 # j_data = json.dumps(data)
-headers = {'content-type': 'text/pain', 'Accept-Charset': 'UTF-8'}
+headers = {'content-type': 'text/plain', 'Accept-Charset': 'UTF-8'}
 r = requests.post(url, data=data, headers=headers)
 print(r, r.text)
