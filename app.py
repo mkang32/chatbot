5d6ce3cd536157525525681df7b77caf82c6aec5
Minkyung Kang <minkyung.kang32@gmail.com>
2021-07-08 23:55:51 -0400

    use specific port and host but it doesn't work when get_response() exsits

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from chat.preprocessing import chatbot_response
 from flask import Flask, request, make_response, render_template
+import os
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def home():
+#     data = {'url': 'http://127.0.0.1:5000/api/'}
+#     return render_template('home.html', data=data)
+
 @app.route('/')
 def home():
-    data = {'url': 'http://127.0.0.1:5000/api/'}
-    return render_template('home.html', data=data)
+    return render_template('test.html')
 
 @app.route('/api/', methods=['POST'])
 def get_response():
@@ -20,4 +25,5 @@ def get_response():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
